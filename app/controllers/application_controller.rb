8e97b3a4f5cb4f4a84f15daa352abc8d8cf3469e
mido149z <minhkk149@gmail.com>
2018-10-10 08:09:18 +0700

    M push code lai

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
 	protect_from_forgery with: :exception
 
 	def intro
-		render html: "Haha"
+		render html: "Sao khong PUSH code len github duoc"
 		
 	end
 end
